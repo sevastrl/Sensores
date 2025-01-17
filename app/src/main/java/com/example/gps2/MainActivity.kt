62cb93cdcb4975952c61a1a267db93b64ec3e1b7
sevastrl <sebastianhcv5@gmail.com>
2025-01-16 22:12:13 -0500

    Primer commit

---
+++ app/src/main/java/com/example/gps2/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.gps2
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.gps2.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var tvLatitude: TextView
+    private lateinit var tvLongitude: TextView
+    private lateinit var tvGravimeter: TextView
+    private lateinit var btnGetLocation: Button
+
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvLatitude = findViewById(R.id.tv_latitude)
+        tvLongitude = findViewById(R.id.tv_longitude)
+        tvGravimeter = findViewById(R.id.tv_gravimeter)
+        btnGetLocation = findViewById(R.id.btn_get_location)
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+
+        btnGetLocation.setOnClickListener {
+            checkLocationPermission()
+        }
+    }
+
+
+}
